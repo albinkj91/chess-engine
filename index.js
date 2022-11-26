5cfece49e0d8d64d4ee66f5b8f8b9679d2022bac
Albin Kjellson <albinkj91@yahoo.com>
2022-11-26 23:23:48 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,229 @@
+const bottom = document.getElementById('bottom');
+const board = document.getElementById('board');
+
+const WHITE = 0;
+const BLACK = 1;
+const blackHex = '#502020';
+const whiteHex = '#eeddaa';
+const PAWN = 'pawn';
+const ROOK = 'rook';
+const KNIGHT = 'knight';
+const BISHOP = 'bishop';
+const QUEEN = 'queen';
+const KING = 'king';
+let turn = WHITE;
+let selectedPiece;
+let whitePawns = [];
+let blackPawns = [];
+let x = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+let currentColor = whiteHex;
+
+function Piece(type, rank, file, color) {
+	this.type = type,
+	this.rank = rank,
+	this.file = file,
+	this.color = color
+};
+
+for(let i = 0; i < 8; i++){
+	whitePawns[i] = new Piece(PAWN, 1, i, WHITE);
+	blackPawns[i] = new Piece(PAWN, 6, i, BLACK);
+}
+
+const boardState = [
+	[
+		new Piece(ROOK, 0, 0, WHITE),
+		new Piece(KNIGHT, 0, 1, WHITE),
+		new Piece(BISHOP, 0, 2, WHITE),
+		new Piece(KING, 0, 3, WHITE),
+		new Piece(QUEEN, 0, 4, WHITE),
+		new Piece(BISHOP, 0, 5, WHITE),
+		new Piece(KNIGHT, 0, 6, WHITE),
+		new Piece(ROOK, 0, 7, WHITE)
+	],
+	[0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0],
+	[
+		new Piece(ROOK, 7, 0, BLACK),
+		new Piece(KNIGHT, 7, 1, BLACK),
+		new Piece(BISHOP, 7, 2, BLACK),
+		new Piece(KING, 7, 3, BLACK),
+		new Piece(QUEEN, 7, 4, BLACK),
+		new Piece(BISHOP, 7, 5, BLACK),
+		new Piece(KNIGHT, 7, 6, BLACK),
+		new Piece(ROOK, 7, 7, BLACK)
+	]
+];
+
+whitePawns.forEach(p => boardState[p.rank][p.file] = p);
+blackPawns.forEach(p => boardState[p.rank][p.file] = p);
+
+const movePiece = (piece, destRank, destFile) =>{
+	if(piece !== 0){
+		boardState[destRank][destFile] = piece;
+		boardState[piece.rank][piece.file] = 0;
+		piece.rank = destRank;
+		piece.file = destFile;
+	}
+};
+
+const getValidKingMoves = (rank, file) =>{
+	let moves = [];
+	if(rank > 0)				moves.push({rank: rank - 1, file: file});
+	if(rank < 7)				moves.push({rank: rank + 1, file: file});
+	if(file > 0)				moves.push({rank: rank, file: file - 1});
+	if(file < 7)				moves.push({rank: rank, file: file + 1});
+	if(rank > 0 && file > 0)	moves.push({rank: rank - 1, file: file - 1});
+	if(rank < 7 && file < 7)	moves.push({rank: rank + 1, file: file + 1});
+	if(rank > 0 && file < 7)	moves.push({rank: rank - 1, file: file + 1});
+	if(rank < 7 && file > 0)	moves.push({rank: rank + 1, file: file - 1});
+
+	moves = moves.filter(x => boardState[x.rank][x.file] === 0 || boardState[x.rank][x.file].color !== turn);
+	return moves;
+};
+
+const highlightValidMoves = (moves) =>{
+	console.log('Moves:', moves);
+	const temp = Array.of(board.childNodes)[0];
+	const moveIds = moves.map(x => `${x.rank}${x.file}`);
+
+	temp.forEach(x => {
+		if(moveIds.includes(x.id)){
+			const mark = document.createElement('div');
+			mark.setAttribute('class', 'mark');
+			x.appendChild(mark);
+			console.log(x);
+		}
+	});
+};
+
+const getValidMoves = (piece) =>{
+	const rank = piece.rank;
+	const file = piece.file;
+	let moves = [];
+
+	switch(piece.type){
+		case KING:
+			moves = getValidKingMoves(rank, file);
+			break;
+		default:
+			console.log('Moves for this piece type not yet implemented');
+	}
+	highlightValidMoves(moves);
+	return moves;
+};
+
+const highlightTile = (element, color) =>{
+	element.style.backgroundColor = color;
+};
+
+const markTile = (element, color) =>{
+	element.style.boxShadow = '4px 4px 4px black';
+	element.style.border = `4px solid ${color}`;
+	element.style.borderRadius = '3px';
+};
+
+const unmarkPiece = (piece) =>{
+	piece.style.boxShadow = '';
+	piece.style.border = '';
+	piece.style.borderRadius = '';
+};
+
+const configPiece = (htmlElement, piece) =>{
+	const img = document.createElement('img');
+	htmlElement.addEventListener('mouseenter', () => {
+		markTile(img, '#308080');
+	});
+	htmlElement.addEventListener('mouseleave', () => {
+		unmarkPiece(img);
+	});
+	htmlElement.appendChild(img);
+	if(piece.color === 0){
+		img.src = `./assets/${piece.type}_white.png`
+	}else{
+		img.src = `./assets/${piece.type}_black.png`
+
+	}
+	return htmlElement;
+};
+
+const clearBoard = () =>{
+	board.innerHTML = '';
+};
+
+const validClick = (piece) =>{
+	return piece.color === turn
+};
+
+const swapTurn = () =>{
+	if(turn === WHITE){
+		turn = BLACK;
+	}else{
+		turn = WHITE;
+	}
+};
+
+const tileClick = (tile) =>{
+	const rank = parseInt(tile.id[0]);
+	const file = parseInt(tile.id[1]);
+	let allowedMoves = [];
+
+	if(selectedPiece === undefined){
+		selectedPiece = boardState[rank][file];
+		console.log('Clicked piece:', selectedPiece);
+		if(!validClick(selectedPiece)){
+			selectedPiece = undefined;
+		}else{
+			tile.style.backgroundColor = '#54AC63';
+			allowedMoves = getValidMoves(selectedPiece);
+		}
+	}
+	else if(allowedMoves.includes({rank: rank, file: file})){
+		movePiece(selectedPiece, rank, file);
+		swapTurn();
+		selectedPiece = undefined;
+		clearBoard();
+		renderBoard();
+	}else{
+		selectedPiece = undefined;
+		clearBoard();
+		renderBoard();
+	}
+};
+
+const renderBoard = () =>{
+	console.log('BOARD_RENDER');
+	for(let i = 0; i < 8; i++){
+		for(let j = 0; j < 8; j++){
+			let tile = document.createElement('div');
+			tile.className = 'tile';
+			tile.id = `${i}${j}`;
+			if(boardState[i][j] !== 0){
+				tile = configPiece(tile, boardState[i][j]);
+			}
+
+			tile.style.backgroundColor = currentColor;
+			if(currentColor === blackHex){
+				currentColor = whiteHex;
+			}else{
+				currentColor = blackHex;
+			}
+
+			tile.addEventListener('click', () => {
+				tileClick(tile);
+			});
+			board.appendChild(tile);
+		}
+		if(currentColor === blackHex){
+			currentColor = whiteHex;
+		}else{
+			currentColor = blackHex;
+		}
+	}
+};
+
+renderBoard();
